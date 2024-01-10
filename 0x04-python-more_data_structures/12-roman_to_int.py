a8a15c6992c14d53dcd35f7cba17492c0a60af2c
edsonlimeme <ed.limeme@gmail.com>
2024-01-10 08:14:34 -0800

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -11,15 +11,3 @@ rep += numbers[i]
 else:
 rep -= numbers[i]
 return rep
-Advance task
-vi 100-weight_average.py
-#!/usr/bin/python3
-def weight_average(my_list=[]):
-if not my_list:
-return 0
-num = 0
-den = 0
-for tup in my_list:
-num += tup[0] * tup[1]
-den += tup[1]
-return (num / den)
